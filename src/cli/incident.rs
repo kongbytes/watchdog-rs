15df060ef499c8f2244b25149e92ae91f47dd14f
Saluki <saluki@gmx.fr>
2023-06-11 18:00:49 +0200

    More details in incident CLI

--- src/cli/incident.rs
+++ src/cli/incident.rs
@@ -2,6 +2,14 @@ use crate::common::error::Error;
 use crate::server::storage::IncidentItem;
 use super::utils::{api_get, format_timestamp};
 
+fn get_error_message(error_message: &Option<String>) -> String {
+
+    match error_message {
+        Some(message) => message.to_string(),
+        None => "no_message".to_string()
+    }
+}
+
 pub async fn list_incidents(base_url: &str, token: &str) -> Result<(), Error> {
 
     let raw_incidents: Vec<IncidentItem> = api_get(base_url, token, "api/v1/incidents").await?;
@@ -37,8 +45,7 @@ pub async fn list_incidents(base_url: &str, token: &str) -> Result<(), Error> {
 
     for incident in incidents.iter() {
     
-        let empty_message = "no_message".to_string();
-        let error_message = incident.error_message.as_ref().unwrap_or(&empty_message);
+        let error_message = get_error_message(&incident.error_message);
         println!("| {: <4} | {: <h_max$} | {: <v_max$} | {: <e_max$} |", incident.id, incident.timestamp, incident.message, error_message, h_max=timestamp_length, v_max=message_length, e_max=error_length);
     }
     println!();
@@ -55,8 +62,9 @@ pub async fn inspect_incident(base_url: &str, token: &str, incident_id: &str) ->
 
     println!();
     println!("Incident ID\t{}", incident.id);
-    println!("Name\t\t{}", incident.message);
     println!("Timestamp\t{}", incident.timestamp);
+    println!("Message\t\t{}", incident.message);
+    println!("Details\t\t{}", get_error_message(&incident.error_message));
     println!();
 
     Ok(())
